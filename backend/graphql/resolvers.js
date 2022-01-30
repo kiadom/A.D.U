60486ff137fe6219b38efc3cb0dbd94f61cc529f
kiadom <kiadom777@gmail.com>
2022-01-29 22:19:23 -0500

    l

--- backend/graphql/resolvers.js
+++
@@ -1,7 +0,0 @@
-import { resolversUsuario } from "../models/usuario/resolvers.js";
-import { resolversProyecto } from "../models/proyecto/resolvers.js";
-import { resolversInscripcion } from "../models/inscripcion/resolvers.js";
-import { resolversAvance } from "../models/avance/resolvers.js";
-import { resolversAutenticacion } from './autenticacion/resolvers.js';
-
-export const resolvers = [resolversUsuario, resolversProyecto, resolversInscripcion, resolversAvance, resolversAutenticacion];
